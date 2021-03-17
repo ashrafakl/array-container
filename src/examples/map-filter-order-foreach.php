8d3a2d321eb771c9291d275f50b886d4a5a318a1
Ashraf Akl <ashrafakl@yahoo.com>
2021-03-17 23:32:01 +0200

    Modify example to use chained method together in a single statement

--- src/examples/map-filter-order-foreach.php
+++ src/examples/map-filter-order-foreach.php
@@ -1,8 +1,10 @@
 <?php
 
+require_once "../ArrayContainer.php";
+
 use ashrafakl\tools\arrays\ArrayContainer;
 
-$array = (new ArrayContainer([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+(new ArrayContainer([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
     ->map(function ($val) {
         return pow(2, $val);
     })
@@ -13,7 +15,7 @@ $array = (new ArrayContainer([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
         array_multisort($list, SORT_DESC, SORT_REGULAR);
         return $list;
     })
-    ->unshift(5, 9);
-$array->forEach(function ($value, $index) {
+    ->unshift(5, 9)
+    ->forEach(function ($value, $index) {
     echo "{$index}|{$value}" . PHP_EOL;
 });
